b889e77460c87a5467854d854dd3d3c0c0b8679a
antlopez33 <129309944+antlopez33@users.noreply.github.com>
2025-03-12 19:39:30 +0000

    Updated congress.py with slack message

--- congress.py
+++ congress.py
@@ -1,12 +1,36 @@
 import os
 import json
 import requests
+from datetime import datetime
+from slack import WebClient
+from slack.errors import SlackApiError
 
+slack_token = os.environ.get('SLACK_API_TOKEN')
 congress_token = 'JxDNj3v4p1OAcaEy5UeOljJljyCWtUKcNkKDXYVp'
+client = WebClient(token=slack_token)
 
-url = f'https://api.congress.gov/v3/committee-report/118/hrpt?api_key={congress_token}'
-
+url = f'https://api.congress.gov/v3/committee-report/119/hrpt?api_key={congress_token}'
 r = requests.get(url)
-
 results = r.json()
-print(results['reports'][0]['url'])
+
+first_result = results['reports'][0]
+display_url = f"https://www.congress.gov/congressional-report/{first_result['congress']}th-congress/house-report/{first_result['number']}"
+
+display_date = datetime.strptime(first_result['updateDate'], '%Y-%m-%dT%H:%M:%SZ')
+formatted_date = display_date.strftime('%B %-d, %Y at %-I:%M%p')
+sentence = f"On {formatted_date}, the House published {first_result['citation']}, which is available at {display_url}"
+
+msg = sentence
+
+try:
+    response = client.chat_postMessage(
+        channel="slack-bots",
+        text=msg,
+        unfurl_links=True, 
+        unfurl_media=True
+    )
+    print("success!")
+except SlackApiError as e:
+    assert e.response["ok"] is False
+    assert e.response["error"]
+    print(f"Got an error: {e.response['error']}")
